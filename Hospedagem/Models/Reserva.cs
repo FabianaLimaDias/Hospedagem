6def8c19bd91986443bc422ad98ff00c08ac1aa6
FabianaLimaDias <fabianacl.dias@gmail.com>
2024-01-31 15:09:10 -0300

    Add project files.

---
+++ Hospedagem/Models/Reserva.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospedagem.Models
+{
+    public class Reserva
+    {
+
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+           
+            try
+            {
+
+                if (hospedes.Count <= Suite.Capacidade)
+                {
+                    Hospedes = hospedes;
+                }
+                else
+                {
+                    throw new Exception(" Numero maior de hospedes, do que comporta a suite ");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
+        public void CadastrarSuite (Suite suite)
+        {
+            Suite = suite;
+        }
+
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count != null ? Hospedes.Count:0 ;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+           
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m;
+            }
+
+            return valor;
+        }
+
+
+    }
+}
+
---
+++ Hospedagem/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hospedagem.Models;
+
+
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hospede 1");
+Pessoa p2 = new Pessoa(nome: "Hospede 2");
+//Pessoa p3 = new Pessoa(nome: "Hospede 3");
+
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+//hospedes.Add(p3);
+
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+
+Reserva reserva = new Reserva(diasReservados: 5);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
